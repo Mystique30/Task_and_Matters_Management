d75a67e933050cb075fadb0699ad8c84414104fb
Mystique30 <samaraclarenazareth@gmail.com>
2026-09-09 14:49:54 +0530

    Fix PEP 668 in build_files.sh with --break-system-packages and enable WHITENOISE_USE_FINDERS

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 # Media files (user uploads)
 MEDIA_URL = '/media/'
